f9ece2e7bf065764516ec7172a0bb0fd63c061b2
Emad Isaac <emadhany2007@gmail.com>
2025-03-21 14:45:49 +0400

    Initial Commit

---
+++ SpaceTravel/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SpaceTravel
+//
+//  Created by Emad_OS_TEST on 21/03/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SpaceTravel/SpaceTravelApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpaceTravelApp.swift
+//  SpaceTravel
+//
+//  Created by Emad_OS_TEST on 21/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct SpaceTravelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
